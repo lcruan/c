dfb7d25dd2a715ac32963c8c5987b2e917b0de85
lcruan <1416802331@qq.com>
2026-04-28 10:37:16 +0800

    'include防止重复包含的使用'

--- 096.include指令的用法/main.c
+++ 096.include指令的用法/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "math_ops.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	int x = 10, y = 5;
+	printf("%d + %d = %d\n", x, y, add(x, y));
+	printf("%d - %d = %d\n", x, y, subtract(x, y));
+	
 	return 0;
 }
--- 096.include指令的用法/math_ops.h
+++ 096.include指令的用法/math_ops.h
@@ -1,2 +1,7 @@
+#ifndef MATH_OPS_H__
+#define MATH_OPS_H__
+
 int add(int a, int b);
 int subtract(int a, int b);
+
+#endif
